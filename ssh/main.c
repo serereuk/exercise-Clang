77234affbba905c56d82dcb05b7bd09f9e71e864
serereuk <imssh186@gmail.com>
2018-07-12 11:35:46 +0900

    Initial Commit

---
+++ ssh/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ssh
+//
+//  Created by Wook-Young on 2018. 7. 12..
+//  Copyright © 2018년 Wook-Young. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
